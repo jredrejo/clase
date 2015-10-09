943bb4b520eae3482ed991e6532b5c2c73a171db
José L. Redrejo Rodríguez <jredrejo@gmail.com>
2015-10-09 08:38:00 +0200

    primer commit de la aplicación clase de web2py

---
+++ controllers/prueba.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+def ver():
+    a= 7
+    palabra="hola"
+    word="kk"
+    
+    return dict(valor=a,otro=palabra,word=word)
+    
+    
+    
